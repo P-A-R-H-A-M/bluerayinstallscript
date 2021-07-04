be5a53d2a815401e48b74fabe05829a403915ea3
bluerayoflight <57138006+bluerayoflight@users.noreply.github.com>
2021-07-04 14:45:25 +0400

    add multimc

--- installscriptubuntu21.04.sh
+++ installscriptubuntu21.04.sh
@@ -15,6 +15,7 @@ dpkg --add-architecture i386
 #more repo adding and key adding
 wget -nc https://dl.winehq.org/wine-builds/winehq.key
 apt-key add winehq.key
+curl -sL -o/tmp/multimc.deb https://files.multimc.org/downloads/multimc_1.5-1.deb
 curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
 curl -sL -o/tmp/discord.deb https://dl.discordapp.net/apps/linux/0.0.15/discord-0.0.15.deb
 echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
@@ -25,6 +26,7 @@ wget https://extensions.gnome.org/extension-data/emoji-selectormaestroschan.fr.v
 #installing apps
 apt update
 dpkg --install /tmp/discord.deb
+dpkg --install /tmp/multimc.deb
 apt install -y appimagelauncher gparted neofetch git ranger steam ffmpeg obs-studio v4l2loopback-dkms lutris mpv brave-browser handbrake gnome-tweaks webcamoid openjdk-16-jre-headless spotify-client
 apt install -y --install-recommends winehq-stable
 
