2bf30b42a87574c4631de49c4e3fa0a7987c4624
bluerayoflight <57138006+bluerayoflight@users.noreply.github.com>
2021-07-04 16:28:32 +0400

    Update installscript.sh

--- installscript.sh
+++ installscript.sh
@@ -3,3 +3,6 @@
 #executes the commands that require sudo 
 sudo sh scripts/ubuntu.sh
 
+mkdir ~/Pictures/Wallpapers
+cp desktopbg/desktopbg.png ~/Pictures/Wallpapers/
+gsettings set org.gnome.desktop.background picture-uri ~/Pictures/Wallpapers/desktopbg.png
