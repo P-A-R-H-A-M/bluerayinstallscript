c138d622d6d5e8ae08939d275e6f19dd9b4647fb
bluerayoflight <57138006+bluerayoflight@users.noreply.github.com>
2021-07-05 11:36:40 +0400

    add vscode (nonsnap version cuz snap store sucks)

--- scripts/ubuntu.sh
+++ scripts/ubuntu.sh
@@ -3,7 +3,7 @@
 
 #ppa adding and installing prerequisites
 add-apt-repository ppa:appimagelauncher-team/stable -y
-apt install -y apt-transport-https curl
+apt install -y apt-transport-https curl software-properties-common wget
 add-apt-repository ppa:obsproject/obs-studio -y
 add-apt-repository ppa:qbittorrent-team/qbittorrent-stable -y
 add-apt-repository universe -y
@@ -22,10 +22,12 @@ echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=
 curl -sS https://download.spotify.com/debian/pubkey_0D811D58.gpg | sudo apt-key add - 
 echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
 wget https://extensions.gnome.org/extension-data/emoji-selectormaestroschan.fr.v20.shell-extension.zip
+wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
 
 #installing apps
 apt update
 dpkg --install /tmp/discord.deb
 dpkg --install /tmp/multimc.deb
-apt install -y appimagelauncher qbittorrent virtualbox libqt5concurrent5 gparted neofetch git ranger steam ffmpeg obs-studio v4l2loopback-dkms lutris mpv brave-browser handbrake gnome-tweaks webcamoid openjdk-16-jre-headless spotify-client
+apt install -y appimagelauncher code qbittorrent virtualbox libqt5concurrent5 gparted neofetch git ranger steam ffmpeg obs-studio v4l2loopback-dkms lutris mpv brave-browser handbrake gnome-tweaks webcamoid openjdk-16-jre-headless spotify-client
 apt install -y --install-recommends winehq-stable
